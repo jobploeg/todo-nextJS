f416eba2b8a5d650ab96d9c0b17c176862e12e62
jploeg <jploeg3450@student.landstede.nl>
2023-02-01 14:12:36 +0100

    pages are responsive now

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,22 +3,22 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
+        <nav className="px-2 sm:px-4 py-2.5 bg-gradient-to-l from-white via-blue-50 to-blue-300">
             <div className="container flex flex-wrap items-center justify-between">
                 <div className="w-full md:w-auto my-4">
-                    <ul className="flex flex-col border border-gray-500 md:border-0 rounded-lg md:flex-row md:font-medium">
+                    <ul className="flex md:flex-row md:border-0 rounded-lg md:flex-row">
                         <li className={"hover:scale-110"}>
-                            <Link href="/" className={"my-2 mx-8 text-gray-600 hover:text-gray-900 text-lg"}>
+                            <Link href="/" className={"my-2 mx-8 hover:text-gray-900 text-xl md:text-lg"}>
                                 Todos
                             </Link>
                         </li>
                         <li className={"hover:scale-110"}>
-                            <Link href="/completed" className={"my-2 mx-8 text-gray-600 hover:text-gray-900 text-lg"}>
+                            <Link href="/completed" className={"my-2 mx-8 hover:text-gray-900 text-xl md:text-lg"}>
                                 Completed
                             </Link>
                         </li>
                         <li className={"hover:scale-110"}>
-                            <Link href="/deleted" className={"my-2 mx-8 text-gray-600 hover:text-gray-900 text-lg"}>
+                            <Link href="/deleted" className={"my-2 mx-8 hover:text-gray-900 text-xl md:text-lg"}>
                                 Deleted
                             </Link>
                         </li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Todos } from '../pages/api/getTodos';
 
 export default function Home() {
     const [input, setInput] = useState([]);
     const [todos, setTodos] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState('');
 
     useEffect(() => {
         setIsLoading(true);
@@ -68,7 +66,6 @@ export default function Home() {
         } else {
             validation_text.innerHTML = '<p style="color: red;"></p>';
             setInput('');
-            //execute function addnewtodo .then to update state
             addNewTodo(input)
         }
     }
@@ -78,26 +75,35 @@ export default function Home() {
 
     return (
     <>
-        <form onSubmit={handleSubmit} className={"mt-16"} >
-            <div className="validation flex justify-center mt-24 mb-1"></div>
+        <form onSubmit={handleSubmit} >
+            <div className="validation flex justify-center mt-20 mb-1"></div>
             <div className={"flex justify-center"}>
                 <input
                     type="text"
                     onChange={(e) => setInput(e.target.value)}
                     value={input}
-                    className={"border border-gray-600 rounded-l w-1/5 h-14 text-2xl pl-1"}
+                    className={"border border-gray-600 rounded-l md:w-1/5 h-14 text-2xl shadow"}
                 />
                 <button onClick={(event) => handleSubmit(event)} className={"border border-gray-600 hover:bg-gray-300 rounded-r px-5 bg-gray-200"}>Add</button>
             </div>
         </form>
         <div className={"flex justify-center"}>
-            <ul className={"flex mt-28 flex-wrap w-2/5 gap-10 justify-center"}>
+            <ul className={"flex flex-col mt-14 justify-center"}>
                 {todos.map((todo) => (
                     <div className={"flex"} key={todo.id}>
-                        <div onClick={() => deleteTodoHandler(todo.id)} className={"my-1 text-2xl hover:line-through font-light  hover:scale-105 hover:cursor-pointer "}>
+                        <p className={"my-1 text-3xl font-light hover:cursor-pointer w-64"}>
                             {todo.title}
-                        </div>
-                        <div onClick={() => completedTodoHandler(todo.id)}  className={"font-extralight rounded py-1 text-xl mt-1 px-4 hover:cursor-pointer hover:scale-125"}> &#10003; </div>
+                        </p>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 hover:scale-110 mt-3 ml-10" onClick={() => completedTodoHandler(todo.id)}>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 hover:scale-110 mt-3 ml-3" onClick={() => deleteTodoHandler(todo.id)} >
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"/>
+                        </svg>
+
+
                     </div>
                 ))}
             </ul>
